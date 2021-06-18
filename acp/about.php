58f4619cc1d2dd033f5a6a274a23ec812e56220d
wordstyle <52134979+wordstyle@users.noreply.github.com>
2021-06-19 01:24:44 +0700

    Update about.php

--- acp/about.php
+++ acp/about.php
@@ -1,7 +1,7 @@
 <h1>::  About XRS ::</h1>
 <hr />
 <p>
-	<i>"This CMS is made for all cracking crews, individuals, for all who keep the scene alive.<br />
+	<i>"This CMS is made for all, individuals, for all who keep the scene alive.<br />
 	And mainly for all guys who are site operators and are not good at PHP coding (cheers my friends!)<br />
 	This kind of release had to be done because... nobody thought to make a release portal in CMS.<br />
 	If you dont like the code/design just edit it, i have tried to make the PHP more simple than possible, with no vulnerability problem, clean and light code etc...<br />
